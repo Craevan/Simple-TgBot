4d591b6ec3afe22de9f60d4973085c3f42565050
Crevan <a.siverskii@gmail.com>
2022-07-21 20:26:22 +0300

    init commit

---
+++ src/main/java/ru/crevan/simpletgbot/SimpleTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.crevan.simpletgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/crevan/simpletgbot/SimpleTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.crevan.simpletgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
