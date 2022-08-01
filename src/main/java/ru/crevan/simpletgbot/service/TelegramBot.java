81e2a73db6c9ee4bd9a3845a76b773d438205b26
Crevan <a.siverskii@gmail.com>
2022-08-01 21:08:38 +0300

    [update] add method for user registration  in DataBase

--- src/main/java/ru/crevan/simpletgbot/service/TelegramBot.java
+++ src/main/java/ru/crevan/simpletgbot/service/TelegramBot.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.crevan.simpletgbot.config.BotConfig;
+import ru.crevan.simpletgbot.model.User;
+import ru.crevan.simpletgbot.model.UserRepository;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,10 +32,13 @@ public class TelegramBot extends TelegramLongPollingBot {
             Type /mydata to see data stored about yourself
 
             Type /help to see this message again""";
-    final BotConfig config;
 
-    public TelegramBot(final BotConfig config) {
+    private final UserRepository userRepository;
+    private final BotConfig config;
+
+    public TelegramBot(final BotConfig config, UserRepository userRepository) {
         this.config = config;
+        this.userRepository = userRepository;
         List<BotCommand> listOfCommands = new ArrayList<>();
         listOfCommands.add(new BotCommand("/start", "get a welcome message"));
         listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
@@ -62,6 +69,22 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void registerUser(final Message message) {
+        if (userRepository.findById(message.getChatId()).isEmpty()) {
+            var chatId = message.getChatId();
+            var chat = message.getChat();
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("User saved: " + user);
+        }
+    }
+
     @Override
     public String getBotToken() {
         return config.getToken();
@@ -74,6 +97,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
             switch (messageText) {
                 case ("/start"):
+                    registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
                 case ("/help"):
