30bbf3f706a91265aedbc1cf860b0ccb5c7c4358
Crevan <a.siverskii@gmail.com>
2022-08-01 20:15:39 +0300

    add BotInitializer class

---
+++ src/main/java/ru/crevan/simpletgbot/config/BotInitializer.java
@@ -0,0 +1,31 @@
+package ru.crevan.simpletgbot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.crevan.simpletgbot.service.TelegramBot;
+
+@Slf4j
+@Component
+public class BotInitializer {
+
+    final TelegramBot telegramBot;
+
+    public BotInitializer(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(telegramBot);
+        } catch (TelegramApiException telegramApiException) {
+            log.error("Error occurred: " + telegramApiException.getMessage());
+        }
+    }
+}
